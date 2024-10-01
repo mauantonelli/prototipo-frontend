005e72e3ca36e1955b9b8015824b4c1679ede036
mauantonelli <pablojuanang@gmail.com>
2024-10-01 14:47:51 -0300

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
-function adicionarProduto() {
-    alert('Funcionalidade de adicionar produto simulada!');
-}
+document.querySelectorAll('.adicionar').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Funcionalidade de adicionar produto simulada!');
+    });
+});
 
-function editarProduto() {
-    alert('Funcionalidade de editar produto simulada!');
-}
+document.querySelectorAll('.editar').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Funcionalidade de editar produto simulada!');
+    });
+});
 
-function removerProduto() {
-    alert('Funcionalidade de remover produto simulada!');
-}
+document.querySelectorAll('.remover').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Funcionalidade de remover produto simulada!');
+    });
+});
